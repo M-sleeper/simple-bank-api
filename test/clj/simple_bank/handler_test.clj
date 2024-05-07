acaca71e655693c97bf5791e3c9ad4c89cd1b64a
Boldizsár Rácz <boldizsar.racz@flexiana.com>
2024-05-07 14:25:54 +0200

    Add test fixtures for db and system

--- test/clj/simple_bank/handler_test.clj
+++ test/clj/simple_bank/handler_test.clj
@@ -1,19 +1,20 @@
 (ns simple-bank.handler-test
   (:require
-   [clojure.test :refer [deftest use-fixtures]]
+   [clojure.test :refer [is deftest use-fixtures]]
    [muuntaja.core :as mc]
    [ring.mock.request :as mock]
-   [simple-bank.test-fixtures :refer [handler with-db]]))
+   [simple-bank.test-fixtures :refer [*handler* with-db with-system]]))
 
-(use-fixtures :each with-db)
+(use-fixtures :each with-db with-system)
 
 (defn <-json [m] (mc/decode "application/json" m))
 
 (defn do-request [{:keys [method endpoint body]}]
-  (handler (-> (mock/request method endpoint)
-               (mock/content-type "application/json")
-               (mock/json-body body))))
+  (*handler* (-> (mock/request method endpoint)
+                 (mock/content-type "application/json")
+                 (mock/json-body body))))
 
 (deftest account-test
   ;; TODO
+  (is (= 1 0))
   )
--- test/clj/simple_bank/test_fixtures.clj
+++ test/clj/simple_bank/test_fixtures.clj
@@ -1,11 +1,25 @@
 (ns simple-bank.test-fixtures
   (:require
+   [clj-test-containers.core :as tc]
    [simple-bank.system :as system]))
 
-(def system (system/start-system!))
-
-(def handler (:simple-bank/app system))
+(def ^:dynamic *datasource* nil)
+(def ^:dynamic *handler* nil)
 
 (defn with-db [f]
-  ;; TODO
-  (f))
+  (let [container
+        (-> (tc/create {:image-name    "postgres"
+                        :exposed-ports [5432]
+                        :env-vars      {"POSTGRES_PASSWORD" "password"
+                                        "POSTGRES_USER" "simple_bank"
+                                        "POSTGRES_DB" "simple_bank"}})
+            (tc/start!))]
+    (f)
+    (tc/stop! container)))
+
+(defn with-system [f]
+  (let [system (system/start-system!)]
+    (binding [*handler* (:simple-bank/app system)
+              *datasource* (:simple-bank.system/db system)]
+      (f))
+    (system/stop-system! system)))
